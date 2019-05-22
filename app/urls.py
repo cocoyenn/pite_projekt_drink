ee5b1b9ba9bd2eb0ea18ff5d259a2878cca19ac4
Karolina Mizera <mizera1112@gmail.com>
2019-05-22 09:38:29 +0200

    Prototype of login and registration mechanism.

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='app-home'),
     path('about/', views.about, name='app-about'),
+    path('login/', views.login, name='app-login'),
+    path('register/', views.register, name='app-register'),
 ]
