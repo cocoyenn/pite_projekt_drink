26c12ae30a1a1435dfd64b1cbda9502ccc60dc7e
Karolina Mizera <mizera1112@gmail.com>
2019-05-28 21:50:30 +0200

    Changed settings of database.

--- djangoFiles/settings.py
+++ djangoFiles/settings.py
@@ -70,18 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'djangoFiles.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
@@ -124,3 +112,16 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
     os.path.join(BASE_DIR, 'profile_pics'),
 )
+
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
+else:
+    # Database
+    # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
