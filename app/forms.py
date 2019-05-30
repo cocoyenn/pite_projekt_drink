26e3371956c7a95607bfd25167aedb76827d0633
Karol Cedro <Kaargul@gmail.com>
2019-05-30 12:15:40 +0200

    updated homepage/login page

--- app/forms.py
+++ app/forms.py
@@ -2,13 +2,16 @@ from django import forms
 from .models import UserProfileInfo
 from django.contrib.auth.models import User
 
+
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
+
     class Meta():
         model = User
-        fields = ('username','first_name', 'last_name','password','email')
+        fields = ('username', 'first_name', 'last_name', 'password', 'email')
+
 
 class UserProfileInfoForm(forms.ModelForm):
-     class Meta():
-         model = UserProfileInfo
-         fields = ('portfolio_site','profile_pic')
+    class Meta():
+        model = UserProfileInfo
+        fields = ('portfolio_site', 'profile_pic')
