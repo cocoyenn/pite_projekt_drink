056bd5ddf3ab86f61b61b04dda2d1a1f3fef2b40
Karolina Mizera <mizera1112@gmail.com>
2019-06-07 17:47:04 +0200

    Added few test cases.

--- app/tests/test_urls.py
+++ app/tests/test_urls.py
@@ -1,9 +1,55 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from app.views import home, about, user_login, user_register, user_logout, makeDrink
 
-# Create your tests here.
-
-class TestUrls(TestCase):
+class TestUrlsFunc(TestCase):
  
-    def test_list_url_is_resolved(self):
-        assert 1 == 2
+    def test_home_url_is_resolved(self):
+        url = reverse('app-home')
+        self.assertEquals(resolve(url).func, home)
+
+    def test_about_url_is_resolved(self):
+        url = reverse('app-about')
+        self.assertEquals(resolve(url).func, about)   
+
+    def test_user_login_url_is_resolved(self):
+        url = reverse('app-login')
+        self.assertEquals(resolve(url).func, user_login)
+
+    def test_user_register_url_is_resolved(self):
+        url = reverse('app-register')
+        self.assertEquals(resolve(url).func, user_register)
+
+    def test_user_logout_url_is_resolved(self):
+        url = reverse('app-logout')
+        self.assertEquals(resolve(url).func, user_logout) 
+
+    def test_make_drink_url_is_resolved(self):
+        url = reverse('app-makeDrink')
+        self.assertEquals(resolve(url).func, makeDrink)
+
+class TestUrlsRouts(TestCase):
+ 
+    def test_home_url_is_resolved(self):
+        url = reverse('app-home')
+        self.assertEquals(resolve(url).route, "app/")
     
+    def test_about_url_is_resolved(self):
+        url = reverse('app-about')
+        self.assertEquals(resolve(url).route, "app/about/")   
+
+    def test_user_login_url_is_resolved(self):
+        url = reverse('app-login')
+        self.assertEquals(resolve(url).route, "app/login/")
+
+    def test_user_register_url_is_resolved(self):
+        url = reverse('app-register')
+        self.assertEquals(resolve(url).route, "app/register/")
+
+    def test_user_logout_url_is_resolved(self):
+        url = reverse('app-logout')
+        self.assertEquals(resolve(url).route, "app/logout") 
+
+    def test_make_drink_url_is_resolved(self):
+        url = reverse('app-makeDrink')
+        self.assertEquals(resolve(url).route, "app/makeDrink/")
