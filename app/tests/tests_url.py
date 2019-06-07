34021234d984767c3f57195ef75836b76d12ce92
Karolina Mizera <mizera1112@gmail.com>
2019-06-07 15:01:06 +0200

    Added skeleton of unit test.

---
+++ app/tests/tests_url.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+class TestUrls(TestCase):
+    def setUp(self):
+
+    def test_list_url_is_resolved(self):
+        assert(1 == 1)
+    
