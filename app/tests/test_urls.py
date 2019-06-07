a2d96513078476ddae2e10272dda09ac164b30cd
Karolina Mizera <mizera1112@gmail.com>
2019-06-07 15:09:33 +0200

    Fixed naming.

--- app/tests/test_urls.py
+++ app/tests/test_urls.py
@@ -3,8 +3,7 @@ from django.test import TestCase
 # Create your tests here.
 
 class TestUrls(TestCase):
-    def setUp(self):
-
+ 
     def test_list_url_is_resolved(self):
-        assert(1 == 1)
+        assert 1 == 2
     
