45121f23619bef429409a98cb47fd151083a5e14
Alicja <akrzeminska1519@gmail.com>
2019-06-08 20:46:38 +0200

    proba testowania ~Ala

--- app/tests/test_models.py
+++ app/tests/test_models.py
@@ -1,1 +1,16 @@
 from django.test import TestCase
+
+from app.models import UserProfileInfo
+from django.contrib.auth.models import User
+
+
+class UserProfileInfo_Test(TestCase):
+
+    def create_UserProfileInfo(self, username):
+        user = User.objects.create(username=username)
+        return UserProfileInfo.objects.create(user=user)
+
+    def test_UserProfileInfo_str(self):
+        upi = self.create_UserProfileInfo("Jan")
+        self.assertEqual(upi.__str__(), "Jan")
+
--- app/tests/test_views.py
+++ app/tests/test_views.py
@@ -1,1 +1,50 @@
 from django.test import TestCase
+
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
+
+class ViewsTest(TestCase):
+
+    def test_home(self):
+        url = reverse("app-home")
+        resp = self.client.get(url)
+
+        self.assertEqual(resp.status_code, 200)
+
+    def test_about(self):
+        url = reverse("app-about")
+        resp = self.client.get(url)
+
+        self.assertEqual(resp.status_code, 200)
+
+
+    def test_user_register(self):
+        url = reverse("app-register")
+        resp = self.client.get(url)
+
+        self.assertEqual(resp.status_code, 200)
+
+
+    def test_user_login(self):
+        url = reverse("app-login")
+        resp = self.client.get(url)
+
+        self.assertEqual(resp.status_code, 200)
+
+
+    def test_makeDrink(self):
+        url = reverse("app-makeDrink")
+        resp = self.client.get(url)
+
+        self.assertEqual(resp.status_code, 200)
+
+
+    def test_user_logout(self):
+        url = reverse("app-logout")
+        resp = self.client.get(url)
+
+        self.assertEqual(resp.status_code, 200)
